4b3c581660e7a3ef88edd0927b169d98b23445a6
bilal shahid <s9iper1@gmail.com>
2015-06-07 18:15:57 +0500

    fix

--- app/src/main/java/com/byteshaft/keyboard/CustomSeekBar.java
+++ app/src/main/java/com/byteshaft/keyboard/CustomSeekBar.java
@@ -12,22 +12,19 @@ import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
- class CustomSeekBar extends ListPreference implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
+ class CustomSeekBar extends ListPreference implements SeekBar.OnSeekBarChangeListener,
+         View.OnClickListener {
 
      private static final String androidns = "http://schemas.android.com/apk/res/android";
      private SeekBar mSeekBar;
      private TextView mSplashText, mValueText;
      private Context mContext;
      int mSeekBarProgress;
-     private String mDialogMessage;
+     CharSequence mTextToDisplay;
+
      public CustomSeekBar(Context context, AttributeSet attrs) {
          super(context, attrs);
          mContext = context;
-         // Get string value for dialogMessage :
-         int mDialogMessageId = attrs.getAttributeResourceValue(androidns, "dialogMessage", 0);
-         if (mDialogMessageId == 0)
-             mDialogMessage = attrs.getAttributeValue(androidns, "dialogMessage");
-         else mDialogMessage = mContext.getString(mDialogMessageId);
      }
 
      @Override
@@ -38,8 +35,6 @@ import android.widget.TextView;
          layout.setPadding(6, 6, 6, 6);
          mSplashText = new TextView(mContext);
          mSplashText.setPadding(30, 10, 30, 10);
-         if (mDialogMessage != null)
-             mSplashText.setText(mDialogMessage);
          layout.addView(mSplashText);
          mValueText = new TextView(mContext);
          mValueText.setGravity(Gravity.CENTER_HORIZONTAL);
@@ -65,7 +60,6 @@ import android.widget.TextView;
      private void setProgressBarValue() {
          final int max = this.getEntries().length - 1;
          mSeekBar.setMax(max);
-         System.out.println(mSeekBarProgress + "new Value");
          mSeekBar.setProgress(mSeekBarProgress);
      }
 
@@ -77,8 +71,9 @@ import android.widget.TextView;
 
      @Override
      public void onProgressChanged(SeekBar seek, int value, boolean fromTouch) {
-         final CharSequence textToDisplay = getEntryFromValue(value);
-         mValueText.setText(textToDisplay);
+         mTextToDisplay = getEntryFromValue(value);
+         mValueText.setText(mTextToDisplay);
+         setSummary(mTextToDisplay);
      }
 
      private CharSequence getEntryFromValue(int value) {
@@ -106,7 +101,6 @@ import android.widget.TextView;
          if (shouldPersist()) {
              final int progressChoice = mSeekBar.getProgress();
              setValueIndex(progressChoice);
-             System.out.println(progressChoice);
              persistString(String.valueOf(progressChoice));
          }
          getDialog().dismiss();
